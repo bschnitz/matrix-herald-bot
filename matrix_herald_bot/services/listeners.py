0365c2d24f4805a331043adde78af4758f9b2f1f
Benjamin Schnitzler <bschnitzler@horstmann-it-consulting.de>
2025-11-11 23:15:36 +0100

    feat: implemented handling of tree structure requests

--- matrix_herald_bot/services/listeners.py
+++ matrix_herald_bot/services/listeners.py
@@ -1,6 +1,8 @@
-import json
-from injector import Module, multiprovider
+from injector import Injector, Module, inject, multiprovider
 from nio import Event, MatrixRoom, UnknownEvent
+from matrix_herald_bot.config.model import Configuration
+from matrix_herald_bot.services.tree_operations import MatrixTreeOperations
+from matrix_herald_bot.services.tree_builder import MatrixTreeBuilder
 
 class ListenerInterface[T: Event]:
     def getEventType(self) -> type[T]:
@@ -10,22 +12,32 @@ class ListenerInterface[T: Event]:
         raise NotImplementedError
 
 class OnTreeRequest(ListenerInterface[UnknownEvent]):
+    @inject
+    def __init__(
+        self,
+        tree_builder: MatrixTreeBuilder,
+        tree_operations: MatrixTreeOperations,
+        config: Configuration
+    ):
+        self.config = config
+        self.tree_builder = tree_builder
+        self.tree_operations = tree_operations
+
     def getEventType(self) -> type[UnknownEvent]:
         return UnknownEvent
 
     async def onEvent(self, room: MatrixRoom, event: UnknownEvent):
         if event.type == 'org.herald.tree_structure_request':
-            print(f"OnTreeRequest")
-            print(json.dumps(event.source, indent=4))
-
-class OnOtherRequest(ListenerInterface[UnknownEvent]):
-    def getEventType(self) -> type[UnknownEvent]:
-        return UnknownEvent
-
-    async def onEvent(self, room: MatrixRoom, event: UnknownEvent):
-        print("onOtherRequest")
+            print("Sending tree to widget.")
+            root = await self.tree_builder.fetch_tree(self.config.watched_spaces[0])
+            await self.tree_operations.send_tree_to_room(
+                root,
+                room.room_id,
+                "org.herald.tree_structure",
+                event.source['state_key']
+            )
 
 class ListenerCollectionModule(Module):
     @multiprovider
-    def provide_my_collection_services(self) -> list[ListenerInterface]:
-        return [OnTreeRequest(), OnOtherRequest()]
+    def provide_my_collection_services(self, injector: Injector) -> list[ListenerInterface]:
+        return [injector.get(OnTreeRequest)]
